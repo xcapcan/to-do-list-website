93edf8fecefb4592e693a97956a18afec40c84b8
Apple <apple@Apples-MacBook-Pro-4.local>
2019-07-26 15:47:37 -0700

    aligned+checkbox

--- index.js
+++ index.js
@@ -2,10 +2,17 @@ var title = $('h1');
 var input = $('input').val();
 var button = $('button');
 var container = $('.container')
+var checkbox = $('.checkbox').val();
 
 button.on('click', addNew);
 
 function addNew() {
   var input = $('input').val();
-container.append(`${input}`);
+  var checkbox = $('.checkbox').val();
+container.append(`
+  <div>
+  <p>${input}</p>
+  <input class="checkbox" type="checkbox"
+  >
+  </div>`);
 }
